767687a952aab939ae3c776aa3100b77b8735e98
gandis0713 <gandis0713@gmail.com>
2025-12-15 23:20:39 +0900

    add litert

--- chopsticks/main.cpp
+++ chopsticks/main.cpp
@@ -1,6 +1,17 @@
 #include <spdlog/spdlog.h>
 
+#include "litert/c/litert_common.h"
+
+#include "litert/c/litert_model.h"
+
 int main(int argc, char* argv[]) {
     spdlog::info("Hello World!");
+
+    LiteRtModel model;
+    LiteRtCreateModelFromFile("model.tflite", &model);
+
+    // LiteRtEnvironment* env = LiteRtCreateEnvironment();
+    // LiteRtDestroyEnvironment(env);
+    
     return 0;
 }
